7631378ad5d9f3f620e8f3a6a0fbcedf57ae25b2
Rohit Kumar <rohitkumar.ak51@gmail.com>
2025-08-16 13:51:21 +0530

    handling infinity

---
+++ Handling Missing Values/infinite.py
@@ -0,0 +1,11 @@
+# np.isinf()
+# 1/0
+
+import numpy as np
+
+arr = np.array([1, 2, np.inf, 4, 5])
+
+print(np.isinf(arr))
+
+cleaned_arr=np.nan_to_num(arr,posinf=1000,neginf=-1000)
+print(cleaned_arr)
